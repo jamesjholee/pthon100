cfdfc9fa40be72a56aaeaefe0a2ce93aa30611cf
James Jaeho Lee <james.lee4789@gmail.com>
2021-02-09 23:16:09 -0500

    day 1

---
+++ d1/main.py
@@ -0,0 +1,42 @@
+# Lesson 1
+print("Hello world!")
+
+# Coding Exercise 1
+print("Day 1 - Python Print Function")
+print("The function is decalred like this:")
+print("print('what to print')")
+
+# Lesson 2
+print("Hello World!\nHello World!\nHello World")
+print("Hello" + " James")
+
+# Coding Exercise 2
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print("e.g. print('Hello ' + 'world')")
+print("New lines can be created with a backslash and n.")
+
+# Lesson 3
+input('What is your name?')
+print("Hello " + input("What is your name?") + "!")
+
+# Coding Exercise 3
+print(len(input("What is your name?")))
+
+# Lesson 4
+name = input("What is your name? ")
+print(name)
+
+# Coding Exercise 4
+a = input("a:")
+b = input("b:")
+
+c = a
+a = b
+b = c
+
+print("a = " + a)
+print("b = " + b)
+
+# Lesson 5
+
