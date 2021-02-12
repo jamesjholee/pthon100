9decf3e4723dd251d19c85084c85b6e2d144f2c9
James Jaeho Lee <james.lee4789@gmail.com>
2021-02-11 23:47:39 -0500

    d3 exercise 2

--- d3/main.py
+++ d3/main.py
@@ -10,3 +10,21 @@
 #     print("Odd")
 
 # D3 Exercise 2
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = round(height/weight**2)
+
+if bmi <= 18.5:
+    print("underweight")
+elif bmi <= 25:
+    print("normal weight")
+elif bmi <= 30: 
+    print("overweight") 
+elif bmi <= 35:
+    print("obese") 
+else:
+    print("clinically obese") 
