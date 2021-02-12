fc05cb01f7af6ecf5212b397a22b3d6eb75aa073
James Jaeho Lee <james.lee4789@gmail.com>
2021-02-11 23:55:02 -0500

    add excercise 3

--- d3/main.py
+++ d3/main.py
@@ -10,21 +10,38 @@
 #     print("Odd")
 
 # D3 Exercise 2
+# # 🚨 Don't change the code below 👇
+# height = float(input("enter your height in m: "))
+# weight = float(input("enter your weight in kg: "))
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# bmi = round(height/weight**2)
+
+# if bmi <= 18.5:
+#     print("underweight")
+# elif bmi <= 25:
+#     print("normal weight")
+# elif bmi <= 30: 
+#     print("overweight") 
+# elif bmi <= 35:
+#     print("obese") 
+# else:
+#     print("clinically obese") 
+
+# D3 Exercise 3
 # 🚨 Don't change the code below 👇
-height = float(input("enter your height in m: "))
-weight = float(input("enter your weight in kg: "))
+year = int(input("Which year do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-bmi = round(height/weight**2)
-
-if bmi <= 18.5:
-    print("underweight")
-elif bmi <= 25:
-    print("normal weight")
-elif bmi <= 30: 
-    print("overweight") 
-elif bmi <= 35:
-    print("obese") 
-else:
-    print("clinically obese") 
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 = 0:
+            print("Leap Year")
+        else:
+            print("Not Leap Year")
+    else:
+        print("Leap Year")
+else: 
+    print('Not Leap Year')
