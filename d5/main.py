c8dfe51397a4533f8640d1ed5fba71012b2e15d6
James Jaeho Lee <james.lee4789@gmail.com>
2021-02-14 17:57:19 -0500

    setup all exercises

---
+++ d5/main.py
@@ -0,0 +1,43 @@
+# D5 Exercise 1
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+#Write your code below this row 👇
+
+# D5 Exercise 2
+# # 🚨 Don't change the code below 👇
+# student_scores = input("Input a list of student scores ").split()
+# for n in range(0, len(student_scores)):
+#   student_scores[n] = int(student_scores[n])
+# print(student_scores)
+# # 🚨 Don't change the code above 👆
+# #Write your code below this row 👇
+
+# D5 Exercise 3
+#Write your code below this row 👇
+
+# D5 Exercise 4
+#Write your code below this row 👇
+
+# D5 Project
+#Password Generator Project
+# import random
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# print("Welcome to the PyPassword Generator!")
+# nr_letters= int(input("How many letters would you like in your password?\n")) 
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# #Eazy Level - Order not randomised:
+# #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+# #Hard Level - Order of characters randomised:
+# #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+
