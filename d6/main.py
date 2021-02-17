3b5c7f50141de34c613c11d7bdaf4709657bb174
James Jaeho Lee <james.lee4789@gmail.com>
2021-02-17 01:16:02 -0500

    d6 exercise 3 done

--- d6/main.py
+++ d6/main.py
@@ -1,22 +1,48 @@
 # D6 Exercise Roboth Hurdle 1 
-# def turn_right():
-#     turn_left()
-#     turn_left()
-#     turn_left()
-# def path():
-#     move()
-#     turn_left()
-#     move()
-#     turn_right()
-#     move()
-#     turn_right()
-#     move()
-#     turn_left()
-# for step in range(6):
-#     path()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def path():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+for step in range(6):
+    path()
 
 # D6 Exercise Roboth Hurdle 2
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    jump()
+
 # D6 Exercise Roboth Hurdle 3
+while not at_goal():
+    if wall_in_fornt():
+        jump()
+    else:
+        move()
+
 # D6 Exercise Roboth Hurdle 4
 # D6 Exercise Roboth Hurdle 5 
 # D6 Project Escape the Maze
