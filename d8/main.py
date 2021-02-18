7b43bb1f708d28e2f06f5d98620a0193e95903f4
James Jaeho Lee <james.lee4789@gmail.com>
2021-02-18 01:28:31 -0500

    decode and encode added

---
+++ d8/main.py
@@ -0,0 +1,69 @@
+
+# D8 Excercise 1
+#Write your code below this line 👇
+# def paint_calc(height, width, cover):
+#     num_of_cans = math.ceil((height * width) / coverage)
+#     print(num_of_cans)
+
+# #Write your code above this line 👆
+# # Define a function called paint_calc() so that the code below works.   
+
+# # 🚨 Don't change the code below 👇
+# test_h = int(input("Height of wall: "))
+# test_w = int(input("Width of wall: "))
+# coverage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverage)
+
+# D8 Excercise 2
+# #Write your code below this line 👇
+# def prime_checker(number):
+#     is_prime = True
+#     for i in range(2, number):
+#         if number % i == 0:
+#             is_prime = False
+#     if is_prime:
+#         print("It's a prime number!")
+#     else:
+#         print("Its not a prime number!")
+
+# #Write your code above this line 👆
+# #Do NOT change any of the code below👇
+# n = int(input("Check this number: "))
+# prime_checker(number=n)
+
+# D8 Project Caesar Cipher 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    cipher_text += alphabet[new_position]
+  print(f"The encoded text is {cipher_text}")
+
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(encrypted_text, shift_amount):
+    decrypted_word = ''
+    for letter in decrypted_word:
+        #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+        idx = alphabet.index(letter) - shift_amount
+        decrypted_word += alphabet[idx]
+    print(decrypted_word)
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(encrypted_text=text, shift_amount=shift)
+else:
+    print("That was not an option!")
