ae33ae2ad6d32a92a6197f6328e5ca891cac5d52
James Jaeho Lee <james.lee4789@gmail.com>
2021-02-19 23:55:34 -0500

    basic calculator finsihed

--- d10/main.py
+++ d10/main.py
@@ -47,13 +47,20 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second  number?: "))
-for symbol in operations:
-    print(symbol)
-
-operation_symbol = input("Pick an operation from the line above: ")
-calc_function = operations[operation_symbol]
-answer = calc_function(num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+def calculator():
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Pick an operation")
+        num2 = float(input("What's the second  number?: "))
+        calc_function = operations[operation_symbol]
+        answer = calc_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        if input(f"Type 'y' to continue calculating with {answer}: ") == "y"
+            num1 = answer
+        else: 
+            should_continue = False
+            calculator()
+calculator()
