30dd0d6cbb46072948ca61d27a45f7792d688e68
James Jaeho Lee <james.lee4789@gmail.com>
2021-02-20 03:13:56 -0500

    exercise 5

--- d13/main.py
+++ d13/main.py
@@ -34,9 +34,10 @@
 # pages = 0
 # word_per_page = 0
 # pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
+# word_per_page = int(input("Number of words per page: "))
 # total_words = pages * word_per_page
 # print(total_words)
+# # the word per page variable had double equals
 
 # #Use a Debugger
 # def mutate(a_list):
