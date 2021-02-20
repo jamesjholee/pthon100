b0fd607295f59a9367839d7fec20eddd3d4c4ff5
James Jaeho Lee <james.lee4789@gmail.com>
2021-02-19 23:27:53 -0500

    finish day 10 exercise 1

---
+++ d10/main.py
@@ -0,0 +1,28 @@
+# D10 Exercise 1
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+        if year % 400 == 0:
+            return True
+        else:
+            return False
+        else:
+        return True
+    else:
+        return False
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+    idx = month - 1
+    if if_leap(year) and month == 2:
+        return 29
+    else:
+        return month_days[idx]
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+
+# D10 Project Calculator
