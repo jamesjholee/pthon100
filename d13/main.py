f9b30fdc21c217df84023e4da117d4c375e36f9e
James Jaeho Lee <james.lee4789@gmail.com>
2021-02-20 03:11:33 -0500

    exercise 4

--- d13/main.py
+++ d13/main.py
@@ -25,9 +25,10 @@
 
 
 # # Fix the Errors
-# age = input("How old are you?")
+# age = inf(input("How old are you?"))
 # if age > 18:
-# print("You can drive at age {age}.")
+#     print(f"You can drive at age {age}.")
+# # f string and indent error as well as change the age to an int
 
 # #Print is Your Friend
 # pages = 0
